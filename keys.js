3ac1bcf4a0b38bb0bfd2343b7ccc8f42efd3095d
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-11-23 14:46:12 -0500

    working spotify

--- keys.js
+++ keys.js
@@ -1,6 +1,24 @@
-console.log('this is loaded');
 
-exports.spotify = {
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
-};
+});
+ 
+spotify
+  .search({ type: 'track', query: 'Boulevard of Broken Dreams', limit: '1'})
+  .then(function(response) {
+    console.log(response.tracks.items[0].artists[0].name);
+
+    console.log(response.tracks.items[0].name);
+    
+    console.log(response.tracks.items[0].external_urls.spotify);
+
+    console.log(response.tracks.items[0].album.name);
+
+
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
--- liri.js
+++ liri.js
@@ -1,17 +1,30 @@
 require("dotenv").config();
-var spotify = new spotify(keys.spotify);
+
 
 var userCommand = process.argv[2];
 
+
+
 if (userCommand === 'concert-this'){
     var userInput = process.argv[3];
+    console.log("test one "+userInput);
+    
 }
 else if (userCommand == 'spotify-this-song'){
     var userInput = process.argv[3];
+    const keys = require('./keys');
+
+
 }
 else if (userCommand == 'movie-this'){
     var userInput = process.argv[3];
+    console.log("test three "+userInput);
+
 }
 else if (userCommand == 'do-what-it-says'){
     var userInput = process.argv[3];
+    console.log("test four "+userInput);
+
 }
+
+
